f1e9d6243b591322074b1da6492f4e7e171f16e5
Matthew Kent <matthew.e.kent@gmail.com>
2013-07-17 21:28:25 -0700

    get aws keys from ENV

--- config.rb
+++ config.rb
@@ -81,4 +81,6 @@ end
 
 activate :s3_sync do |s|
   s.bucket = 'www.aliandmattgetmarried.com'
+  s.aws_access_key_id = ENV['AWS_ACCESS_KEY_ID']
+  s.aws_secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
 end
